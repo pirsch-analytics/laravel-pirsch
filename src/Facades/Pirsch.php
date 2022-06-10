cd8cfd6187742d27595fc7982c971ea5c4c9a9b5
Zep Fietje <hey@zepfietje.com>
2022-06-10 23:24:22 +0200

    Configure template

---
+++ src/Facades/Pirsch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pirsch\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Pirsch extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'pirsch';
+    }
+}
---
+++ src/Pirsch.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Pirsch;
+
+class Pirsch
+{
+}
