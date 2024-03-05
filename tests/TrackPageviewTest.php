d8a68c0f0025e7b72a84adee0264685ba04300db
Alexis Saettler <alexis@saettler.org>
2024-03-05 23:01:46 +0100

    Add tests

---
+++ tests/TrackPageviewTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Pirsch\Facades\Pirsch;
+use Pirsch\Http\Middleware\TrackPageview;
+
+it('handles request', function () {
+    Pirsch::shouldReceive('track')
+        ->once();
+
+    Route::middleware(TrackPageview::class)
+        ->get('/', fn () => 'Hello World');
+
+    $this->get('/');
+
+    expect(true)->toBeTrue();
+});
+
+it('skips redirects', function () {
+    Pirsch::spy();
+
+    Route::middleware(TrackPageview::class)
+        ->get('/', fn () => redirect('/home'));
+
+    $this->get('/');
+
+    Pirsch::shouldNotHaveBeenCalled();
+    expect(true)->toBeTrue();
+});
+
+it('skips livewire', function () {
+    Pirsch::spy();
+
+    Route::middleware(TrackPageview::class)
+        ->get('/', fn () => 'Hello World');
+
+    $this->get('/', ['X-Livewire' => 'true']);
+
+    Pirsch::shouldNotHaveBeenCalled();
+    expect(true)->toBeTrue();
+});
+
+it('skips telescope', function () {
+    Pirsch::spy();
+
+    Route::middleware(TrackPageview::class)
+        ->get('telescope/test', fn () => 'Hello World');
+
+    $this->get('/telescope/test');
+
+    Pirsch::shouldNotHaveBeenCalled();
+    expect(true)->toBeTrue();
+});
