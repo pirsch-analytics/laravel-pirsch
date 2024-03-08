5892d649673718eb814dc1828b6d045769a6ff3f
Alexis Saettler <alexis@saettler.org>
2024-03-08 14:29:31 +0100

    Improve Middleware except

--- src/Http/Middleware/TrackPageview.php
+++ src/Http/Middleware/TrackPageview.php
@@ -9,6 +9,28 @@ use Pirsch\Facades\Pirsch;
 
 class TrackPageview
 {
+    /**
+     * The URIs that should be excluded from tracking.
+     *
+     * @var array<int,string>
+     */
+    protected array $except = [
+        'telescope',
+        'horizon',
+    ];
+
+    /**
+     * The Headers that should be excluded from tracking.
+     *
+     * @var array<int,string>
+     */
+    protected array $exceptHeaders = [
+        'X-Livewire',
+    ];
+
+    /**
+     * Handle an incoming request.
+     */
     public function handle(Request $request, Closure $next): mixed
     {
         $response = $next($request);
@@ -17,16 +39,44 @@ class TrackPageview
             return $response;
         }
 
-        if ($request->hasHeader('X-Livewire')) {
-            return $response;
+        if (! $this->inExceptArray($request) &&
+            ! $this->inExceptHeadersArray($request)
+        ) {
+            Pirsch::track();
         }
-
-        if (str_starts_with($request->route()->uri, 'telescope/')) {
-            return $response;
-        }
-
-        Pirsch::track();
 
         return $response;
+    }
+
+    /**
+     * Determine if the request has a header that should not be tracked.
+     */
+    protected function inExceptHeadersArray(Request $request): bool
+    {
+        foreach ($this->exceptHeaders as $except) {
+            if ($request->hasHeader($except)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the request has a URI that should not be tracked.
+     */
+    protected function inExceptArray(Request $request): bool
+    {
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+
+            if ($request->fullUrlIs($except) || $request->is($except)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
