e659da89c1ae646e503f64b3a8100cac3b65bfc7
Alexis Saettler <alexis@saettler.org>
2025-03-20 20:51:13 +0100

    Fix tests running (#11)

--- src/Facades/Pirsch.php
+++ src/Facades/Pirsch.php
@@ -4,6 +4,11 @@ namespace Pirsch\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void track(?string $name = null, ?array $meta = null)
+ *
+ * @see \Pirsch\Pirsch
+ */
 class Pirsch extends Facade
 {
     protected static function getFacadeAccessor()
